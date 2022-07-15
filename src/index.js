8047290c59aa574fd64392969a34e8780321249a
Svetlana.Safonova <safonova.sv.a@gmail.com>
2022-07-15 15:00:48 +0300

    feat: add functioal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2) {
+    return false;
+  }
+  let strBegin = '';
+  let strEnd = '';
+  for(let i = 0; i < bracketsConfig.length; i++) {
+    strBegin = strBegin + bracketsConfig[i][0];
+    strEnd = strEnd + bracketsConfig[i][1];
+  }
+  if(strBegin.indexOf(str[0]) == -1 || strEnd.indexOf(str[str.length - 1]) == -1) {
+    return false;
+  }
+  let index = 0;
+  while(index < str.length && index >= 0) {
+    let indexBracked = strBegin.indexOf(str[index]);
+    let indexBrackEnd = strEnd.indexOf(str[index]);
+    if(indexBracked >= 0) {
+      if(str[index + 1] == strEnd[indexBracked]) {
+        str = str.substring(0, index) + str.substring(index + 2, str.length);
+        index = index > 0 ? index - 1 : 0;
+      }
+      else {
+        index++;
+      }
+    }
+    else if(indexBrackEnd >= 0) {
+      return false;
+    }
+  }
+  if(str.length > 0) return false;
+  return true;
 }
